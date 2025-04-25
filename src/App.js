732aea2d21d2992f9b6dcc1ca6d8509e00b72163
Grace Pontanar <gracepontanar@gmail.com>
2025-04-25 20:32:44 +0800

    adding files

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// import AppNavbar from './components/AppNavbar';
+import Login from './pages/Login';
+import { UserProvider } from './UserContext';
+// import Home from './pages/Home';
+
+
+function App() {
+
+    const [user, setUser] = useState({
+        id: null,
+        isAdmin: null
+    });
+
+    const unsetUser = () => {
+      localStorage.clear();
+    };
+
+    useEffect(() => {
+
+        fetch(``, {
+            headers: {
+                Authorization: `Bearer ${ localStorage.getItem('token') }`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+          
+            if (typeof data.user !== "undefined") {
+
+                setUser({
+                  id: data.user._id,
+                  isAdmin: data.user.isAdmin
+                });
+
+            } else {
+
+                setUser({
+                  id: null,
+                  isAdmin: null
+                });
+            }
+        })
+    }, []);
+
+    return (
+        <UserProvider value={{user, setUser, unsetUser}}>
+            <Router>
+                {/* <AppNavbar /> */}
+                <Routes>
+                    {/* <Route path="/" element={<Home />} /> */}
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </Router>
+        </UserProvider>
+    );
+}
+
+export default App;
---
+++ src/UserContext.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const UserContext = React.createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    id: null, // Default id value as null
+    isAdmin: false, // Default value for isAdmin
+  });
+
+  // Load user data from localStorage on app initialization
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const unsetUser = () => {
+    localStorage.clear();
+    setUser({
+      id: null,
+      isAdmin: false,
+    });
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser, unsetUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
